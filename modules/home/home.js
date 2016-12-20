7468f1c4919ad497dd1781b64221d9b3af05efdc
liguojie <929200072@qq.com>
2016-12-20 12:47:19 +0800

    font1

--- modules/home/home.js
+++ modules/home/home.js
@@ -249,10 +249,10 @@ define(["text!./home.html","css!./home.css"], function(homePage,temp){
 			        var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
 			        var speed = res.speed; // 速度，以米/每秒计
 			        var accuracy = res.accuracy; // 位置精度
+					alert("经度"+longitude);
+					alert("纬度"+latitude);
 			    }
 			});
-			alert("经度"+longitude);
-			alert("纬度"+latitude);
 		});
 			// 	//开场动画
 			var t = setTimeout(time1, 2000);
