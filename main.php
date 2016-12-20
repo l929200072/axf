87a7f826622b36cf82ab3d3f2c17a3012ac96274
liguojie <929200072@qq.com>
2016-12-20 10:47:28 +0800

    local

--- main.php
+++ main.php
@@ -53,19 +53,19 @@ $signPackage = $jssdk->GetSignPackage();
 	</body>
 		<script type = "text/javascript" src="js/commen.js"></script>
 		<script>
-// 		document.getElementById("inp1").onclick = function(){
-// 			wx.getLocation({
-//     type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
-//     success: function (res) {
-//         var latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
-//         var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
-//         var speed = res.speed; // 速度，以米/每秒计
-//         var accuracy = res.accuracy; // 位置精度
-//         alert("X"+latitude);
-//         alert("X"+longitude);
-//     }
-// });
-		}
+/*		document.getElementById("inp1").onclick = function(){
+			wx.getLocation({
+    type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
+    success: function (res) {
+        var latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
+        var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
+        var speed = res.speed; // 速度，以米/每秒计
+        var accuracy = res.accuracy; // 位置精度
+        alert("X"+latitude);
+        alert("X"+longitude);
+    }
+});
+		}*/
   wx.config({
     debug: true,
     appId: '<?php echo $signPackage["appId"];?>',
